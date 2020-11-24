0236a3ec9b734e5001a0eccba7a058fa5f1f9be1
Unknown <unknown@example.com>
2020-11-24 20:56:06 +0200

    adding other options for loops

--- script.js
+++ script.js
@@ -10,6 +10,34 @@ const personalMovieDB = {
     privat: false
 };
 
+// let i = 0;
+// do {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//           b = prompt("На сколько оцените его?(0-10)", "");
+//           i++;
+//           if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log("done");
+//         } else {
+//             console.log("error");
+//             i--;
+//           }
+// } while (i < 2);
+
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//           b = prompt("На сколько оцените его?(0-10)", "");
+//           i++;
+//           if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log("done");
+//         } else {
+//             console.log("error");
+//             i--;
+//           }         
+        
+// }
 for (let i = 0; i < 2; i++) {
     const a = prompt("Один из последних просмотренных фильмов?", ""),
           b = prompt("На сколько оцените его?(0-10)", "");
